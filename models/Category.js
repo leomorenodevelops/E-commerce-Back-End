47d0c9cd840202c733f258a36a4d475ed278f56a
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-11 15:32:04 -0600

    Defined columns in Category.js

--- models/Category.js
+++ models/Category.js
@@ -11,8 +11,12 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      
-    }
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
