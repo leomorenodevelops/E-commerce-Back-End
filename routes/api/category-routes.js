38111f611767efe9a82c1448cedd03f9556b6cfb
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-09-30 15:33:15 -0600

    Fixed get route for category ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,6 +28,7 @@ router.get('/:id', (req, res) => {
       res.status(404).json({ message: 'No Category was found with that id! '});
       return;
     }
+    res.status(200).json(singleCategoryData);
   } catch (err) {
     res.status(500).json(err);
   }
